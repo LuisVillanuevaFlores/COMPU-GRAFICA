bc5945f7cf4a49f963635f5203db5f591c61da60
LuisVillanuevaFlores <furiadorada@hotmail.com>
2020-04-29 19:15:42 -0500

    treshtrigoupdate

--- tresholding/treshtrigo.py
+++ tresholding/treshtrigo.py
@@ -1,44 +1,19 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-
-img3 = cv2.imread('trigo.png')
-
-
+fig,axs=plt.subplots(1,3)
+img = cv2.imread('trigo.png')
+axs[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 color = ('b','g','r')
-
 for i, c in enumerate(color):
-    hist = cv2.calcHist([img3], [i], None, [256], [0, 256])
-    plt.plot(hist, color = c)
-    plt.xlim([0,256])
-
-height, width , canal= img3.shape
+    hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+    axs[1].plot(hist, color = c)
+height, width , canal= img.shape
 for i in range(height):
     for j in range(width):
-            if img3[i][j][0]<=121 or img3[i][j][1]<=144 or img3[i][j][2]<=184:
-                img3[i][j][0]=0
-                img3[i][j][1]=0
-                img3[i][j][2]=0
-
-'''
-
-if img3[i][j][0]>0 and img3[i][j][0]<46:
-                img3[i][j][0]=0
-                img3[i][j][1]=0
-                img3[i][j][2]=0
-            if img3[i][j][1]>175 and img3[i][j][1]<250:
-                img3[i][j][0]=0
-                img3[i][j][1]=0
-                img3[i][j][2]=0
-            if img3[i][j][1]>210 and img3[i][j][1]<260:
-                img3[i][j][0]=0
-                img3[i][j][1]=0
-                img3[i][j][2]=0      
-                         
-'''
-
-        
-cv2.imshow('trigo',img3)
+            if img[i][j][0]<=121 or img[i][j][1]<=144 or img[i][j][2]<=184:
+                img[i][j][0]=0
+                img[i][j][1]=0
+                img[i][j][2]=0
+axs[2].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.show()
-
-cv2.destroyAllWindows()
